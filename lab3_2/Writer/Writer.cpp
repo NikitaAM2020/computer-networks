137c99876a5864ae6381251ad61f5578b32870c7
NikitaAM2020 <nikita.zhuravel24@gmail.com>
2023-11-20 10:46:58 +0200

    add lab3 part2

---
+++ lab3_2/Writer/Writer.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <windows.h>
+
+int main() {
+    // Створення іменованого каналу для запису (серверний канал)
+    HANDLE hPipeServer = CreateNamedPipe(
+        L"\\\\.\\pipe\\MyNamedPipe", // Ім'я іменованого каналу
+        PIPE_ACCESS_OUTBOUND,       // Доступ для запису
+        PIPE_TYPE_BYTE | PIPE_READMODE_BYTE | PIPE_WAIT,
+        1,                          // Лічильник інстансів
+        0, 0, 0, NULL);
+
+    if (hPipeServer == INVALID_HANDLE_VALUE) {
+        std::cerr << "CreateNamedPipe failed: " << GetLastError() << std::endl;
+        return 1;
+    }
+
+    // Очікування підключення клієнта
+    std::cout << "Waiting for client connection..." << std::endl;
+    if (ConnectNamedPipe(hPipeServer, NULL)) {
+        std::cout << "Client connected." << std::endl;
+
+        // Рядок для відправлення клієнту
+        std::string message = "Hello from the server process!";
+
+        // Відправка рядка клієнту
+        DWORD bytesWritten;
+        if (WriteFile(hPipeServer, message.c_str(), static_cast<DWORD>(message.length()), &bytesWritten, NULL)) {
+            std::cout << "Message sent to client: " << message << std::endl;
+        }
+        else {
+            std::cerr << "Failed to send message: " << GetLastError() << std::endl;
+        }
+
+        DisconnectNamedPipe(hPipeServer);
+    }
+    else {
+        std::cerr << "Failed to connect: " << GetLastError() << std::endl;
+    }
+
+    CloseHandle(hPipeServer);
+
+    return 0;
+}
---
+++ lab3_2/lab3_2/Reader.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <windows.h>
+
+int main() {
+    // Відкриття іменованого каналу для читання (клієнтський канал)
+    HANDLE hPipeClient = CreateFile(
+        L"\\\\.\\pipe\\MyNamedPipe", // Ім'я іменованого каналу
+        GENERIC_READ,               // Доступ для читання
+        0, NULL, OPEN_EXISTING, 0, NULL);
+
+    if (hPipeClient == INVALID_HANDLE_VALUE) {
+        std::cerr << "Error opening named pipe: " << GetLastError() << std::endl;
+        return 1;
+    }
+
+    // Читання рядка з іменованого каналу
+    char buffer[256];
+    DWORD bytesRead;
+    if (ReadFile(hPipeClient, buffer, sizeof(buffer), &bytesRead, NULL)) {
+        buffer[bytesRead] = '\0'; // Додавання завершуючого нуля
+        std::cout << "Message from the server: " << buffer << std::endl;
+    }
+    else {
+        std::cerr << "Error reading data from the named pipe: " << GetLastError() << std::endl;
+    }
+
+    CloseHandle(hPipeClient);
+
+    return 0;
+}
