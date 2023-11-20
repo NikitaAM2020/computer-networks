886a4a178ef4b38a7f690628c4d58b1a52434f3c
NikitaAM2020 <nikita.zhuravel24@gmail.com>
2023-11-20 10:48:50 +0200

    add lab3 part3

---
+++ lab3_3/Reader2/L3-reader.cpp
@@ -0,0 +1,33 @@
+﻿#include <windows.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+    HANDLE hPipe;
+    const wchar_t* pipeName = L"\\\\.\\pipe\\MyNamedPipe"; // Update this with the server's pipe name.
+
+    // Connect to the named pipe.
+    hPipe = CreateFile(pipeName, GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
+
+    if (hPipe == INVALID_HANDLE_VALUE) {
+        cerr << "Failed to connect to the server: " << GetLastError() << endl;
+        return 1;
+    }
+
+    cout << "Connected to the server." << endl;
+
+    // Receive data from the server.
+    char buffer[256] = { 0 };
+    DWORD bytesRead;
+    if (ReadFile(hPipe, buffer, sizeof(buffer), &bytesRead, NULL)) {
+        cout << "Received message from server: " << buffer << endl;
+    }
+    else {
+        cerr << "Failed to read data: " << GetLastError() << endl;
+    }
+
+    // Close the named pipe.
+    CloseHandle(hPipe);
+
+    return 0;
+}
---
+++ lab3_3/Writer/L3-server-client.cpp
@@ -0,0 +1,60 @@
+﻿#include <windows.h>
+#include <iostream>
+#include <thread>
+using namespace std;
+
+void handleClient(HANDLE hPipe) {
+    cout << "Client connected." << endl;
+
+    // Send a message to the client.
+    const char* message = "Hello from the server process!";
+    DWORD bytesWritten;
+    BOOL isWritten = WriteFile(hPipe, message, strlen(message), &bytesWritten, NULL);
+
+    if (isWritten) {
+        cout << "Message sent to client: " << message << endl;
+    }
+    else {
+        cerr << "Failed to send message: " << GetLastError() << endl;
+    }
+
+    // Disconnect and close the named pipe.
+    DisconnectNamedPipe(hPipe);
+    CloseHandle(hPipe);
+}
+
+int main() {
+    while (true) {
+        // Create a named pipe.
+        HANDLE hPipe = CreateNamedPipe(
+            L"\\\\.\\pipe\\MyNamedPipe", // Pipe name.
+            PIPE_ACCESS_DUPLEX,         // Read/Write access.
+            PIPE_TYPE_BYTE | PIPE_READMODE_BYTE | PIPE_WAIT, // Byte-type pipe, blocking mode.
+            PIPE_UNLIMITED_INSTANCES,   // Number of instances.
+            0,                          // Output buffer size.
+            0,                          // Input buffer size.
+            NMPWAIT_USE_DEFAULT_WAIT,   // Time-out interval.
+            NULL);                      // Default security attributes.
+
+        if (hPipe == INVALID_HANDLE_VALUE) {
+            cerr << "CreateNamedPipe failed: " << GetLastError() << endl;
+            return 1;
+        }
+
+        cout << "Waiting for client connection..." << endl;
+
+        // Wait for a client to connect.
+        BOOL isConnected = ConnectNamedPipe(hPipe, NULL) ? TRUE : (GetLastError() == ERROR_PIPE_CONNECTED);
+
+        if (isConnected) {
+            thread clientThread(handleClient, hPipe);
+            clientThread.detach();
+        }
+        else {
+            cerr << "Failed to connect: " << GetLastError() << endl;
+            CloseHandle(hPipe);
+        }
+    }
+
+    return 0;
+}
